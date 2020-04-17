ec9079cdd7443d7b4c636d36b1883aa6f4f941ee
George Williams <gwilliam@ucar.edu>
2020-04-17 17:40:03 -0600

    modified load-certs to use TLS_CERT_NAME, TLS_PRIVATE_KEY

--- test/test-load-certs.sh
+++ test/test-load-certs.sh
@@ -39,7 +39,36 @@ crtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtcrtc
 -----END CERTIFICATE-----
 EOF
 
+DEFINE_TEST "when PARM_DB not set, error"
+RUN load-certs $TMPDIR/test1.d $TMPDIR/testOut.d
+if noOutput &&
+   gotExpectedOutput --error --contains "PARM_DB environment variable must be set"
+then
+    SUCCESS
+else
+    FAILURE
+fi
+
+PARM_DB=$TMPDIR/parmdb export PARM_DB
+mkdir $PARM_DB
+
+parmdb --put=TLS_CERT_NAME=foo.bar1
+
 DEFINE_TEST "when key, crt, and .CA.crt files found, all are copied"
+RUN load-certs $TMPDIR/test1.d $TMPDIR/testOut.d
+if noOutput && \
+   noOutput --error && \
+   fileEquals $TMPDIR/testOut.d/private/foo.bar1.key $TMPDIR/test1.d/foo.bar1.key && \
+   fileEquals $TMPDIR/testOut.d/certs/foo.bar1.crt $TMPDIR/test1.d/foo.bar1.crt && \
+   fileEquals $TMPDIR/testOut.d/certs/foo.bar1.CA.crt $TMPDIR/test1.d/foo.bar1.CA.crt
+then
+    SUCCESS
+else
+    FAILURE
+fi
+
+rm -f $TMPDIR/testOut.d/certs/*
+DEFINE_TEST "when no input key but extant output key, certs copied and key untouched"
 RUN load-certs $TMPDIR/test1.d $TMPDIR/testOut.d
 if noOutput && \
    noOutput --error && \
@@ -54,6 +83,26 @@ fi
 
 rm -rf $TMPDIR/testOut.d
 
+mv $TMPDIR/test1.d/foo.bar1.key $TMPDIR/test1.d/foo.bar1.key.sav
+k=`cat $TMPDIR/test1.d/foo.bar1.key.sav`
+parmdb --put=TLS_PRIVATE_KEY="$k"
+
+DEFINE_TEST "when no key but TLS_PRIVATE_KEY set, certs copied and key created"
+RUN load-certs $TMPDIR/test1.d $TMPDIR/testOut.d
+if noOutput && \
+   noOutput --error && \
+   fileEquals $TMPDIR/testOut.d/private/foo.bar1.key $TMPDIR/test1.d/foo.bar1.key.sav && \
+   fileEquals $TMPDIR/testOut.d/certs/foo.bar1.crt $TMPDIR/test1.d/foo.bar1.crt && \
+   fileEquals $TMPDIR/testOut.d/certs/foo.bar1.CA.crt $TMPDIR/test1.d/foo.bar1.CA.crt
+then
+    SUCCESS
+else
+    FAILURE
+fi
+
+
+parmdb --put=TLS_CERT_NAME=foo.bar2
+
 DEFINE_TEST "when key, bundle.pem files found, pem split and copied with key"
 RUN load-certs $TMPDIR/test2.d $TMPDIR/testOut.d
 if noOutput && \
@@ -66,6 +115,5 @@ then
 else
     FAILURE
 fi
-
 
 . cleanup.rc
